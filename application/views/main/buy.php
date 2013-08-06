421eb24cdfb582313593d4ac701eb49380ce345b
GreeZZly <greezzly7@gmail.com>
2013-08-06 13:00:52 +0400

    servicename translit

--- application/views/main/buy.php
+++ application/views/main/buy.php
@@ -6,13 +6,13 @@
 			<form action='https://merchant.intellectmoney.ru/ru/' name='pay' method='POST'>
 				<input type="hidden" name="eshopId" value="451343">
 				<input type="hidden" name="orderId" value="<?=$value['id']?>"> 
-				<input type="hidden" name="serviceName" value="<?=urldecode(urlencode($value['serviceName']))?>"> 
+				<input type="hidden" name="serviceName" value="<?=$value['serviceName']?>"> 
 				<input type="hidden" name="recipientAmount" value="<?=$value['price']?>"> 
 				<input type="hidden" name="recipientCurrency" value="TST"> 
 				<input type="hidden" name="successUrl" value="http://miloslavskiy.com/main/success"> 
 				<input type="hidden" name="failUrl" value="http://miloslavskiy.com/main/fail">
 			    <input type="hidden" name="expireDate" value="2013-11-30 22:55:00"> 
-			    <input type="text" name="userName" >
+			    <input type="text" name="userName" value="">
 				<input type="submit" name="button" value="КУПИТЬ"  class="buy_button">
 			</form> 
 	</div>
